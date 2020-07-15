06e7d9fa035bc647719b21105e4aa5a0c76c0450
cckn <cckn1985@gmail.com>
2020-07-15 10:40:15 +0900

    기본 동작은 구현하였으나 옵션 설정용 GUI 제작 중 난황을 겪음. Q쓰레드를 적용하기 전 리팩토링 필요

--- main.py
+++ main.py
@@ -1,41 +1,85 @@
+from PyQt5.QtCore import QThread
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support import expected_conditions as EC
+from PyQt5.QtWidgets import QApplication, QLabel
 
-import pyautogui as pag
 import keyboard
 
 import time
-import threading
 
-# This example requires Selenium WebDriver 3.13 or newer
-
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 980
+import web_config
+import gui
 
 driver = webdriver.Chrome()
-driver.set_window_position(1920 / 2 - 170, 0)
-driver.set_window_size(1920 / 2 + 170, 1080)
-# with as driver:
-# wait = WebDriverWait(driver, 600)
-driver.get("https://www.hometax.go.kr/")
 
 
-# first_result = wait.until(presence_of_element_located(By.CSS_SELECTOR, "h3>div"))
-# print(first_result.get_attribute("textContent"))
+class Worker(QThread):
+    def run(self):
+        init()
+        main()
+
+
+def init():
+    init_qt()
+    gui.init()
+    init_chrome()
+
+
+def init_chrome():
+    SCREEN_WIDTH = 1920
+    SCREEN_HEIGHT = 980
+
+    driver.set_window_position(SCREEN_WIDTH / 2 - 170, 0)
+    driver.set_window_size(SCREEN_WIDTH / 2 + 170, SCREEN_HEIGHT)
+    driver.get("https://www.hometax.go.kr/")
+
+
+def init_qt():
+    app = QApplication([])
+    label = QLabel('Hello World!')
+    label.show()
+
+
+def main():
+    while True:
+        if is_alert_present():
+            alert_handler()
+
+        if keyboard.is_pressed("F4"):
+            action_goto_new()
+            print("F7")
+
+        if keyboard.is_pressed("F7"):
+            action_goto_list()
+            print("F7")
+
+        if keyboard.is_pressed("F8"):
+            print("F8")
+
+        if keyboard.is_pressed("F9"):
+            print("F9")
+
+        if keyboard.is_pressed("F10"):
+            print("F10")
+            logout_and_login()
+
+        time.sleep(0.01)
+        # print(web_config.config.wanna_alert_kill)
+
 
 def is_alert_present():
     try:
-        alert = driver.switch_to.alert
+        check_alert = driver.switch_to.alert  # check Alert
         return True
     except Exception:
         return False
 
 
-def alert_close():
+def alert_handler():
     alert = driver.switch_to.alert
     text = alert.text
 
@@ -74,7 +118,7 @@ def logout_and_login():
         # logout button click
         hometax_logout()
         # confirm alert close
-        alert_close()
+        alert_handler()
         # wait
         wait = WebDriverWait(driver, 1000)
         wait.until(EC.presence_of_element_located((By.LINK_TEXT, "로그인")))
@@ -82,33 +126,28 @@ def logout_and_login():
     hometax_login()
 
 
+def action_goto_new():
+    try:
+        button_click(driver.find_element(By.ID, "menuAtag_0104010100"))
+    except:
+        print("F10 Except")
 
-def action_go_total_page():
+
+def action_goto_total_page():
     try:
         button_click(driver.find_element(By.ID, "menuAtag_0104030200"))
     except:
         print("F10 Except")
 
 
-# alertKillerT = threading.Thread(target=is_alert_present)
-# alertKillerT.start()
-while True:
-    if is_alert_present():
-        alert_close()
+def action_goto_list():
+    try:
+        button_click(driver.find_element(By.ID, "menuAtag_0104020100"))
+    except:
+        print("F10 Except")
 
-    if keyboard.is_pressed("F4"):
-        action_go_total_page()
 
-    if keyboard.is_pressed("F7"):
-        logout_and_login()
-
-    if keyboard.is_pressed("F8"):
-        print("F8")
-
-    if keyboard.is_pressed("F9"):
-        print("F9")
-
-    if keyboard.is_pressed("F10"):
-        print("F10")
-
-    time.sleep(0.01)
+if __name__ == "__main__":
+    # execute only if run as a script
+    init()
+    main()
---
+++ web_config.py
@@ -0,0 +1,7 @@
+class WebConfig:
+    def __init__(self):
+        self.wanna_alert_kill = False
+
+
+config = WebConfig()
+
